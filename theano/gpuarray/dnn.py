6ec7568213cc1b169fbf1ede151e7fc6dfd4e0e5
sentient07 <vxrram95@gmail.com>
2016-11-23 23:41:29 +0100

    Added GPUOps for RoIPool and RoiPoolGrad Ops

--- theano/gpuarray/dnn.py
+++ theano/gpuarray/dnn.py
@@ -27,7 +27,7 @@ from theano.tensor.nnet.abstract_conv import (AbstractConv2d,
                                               get_conv_output_shape,
                                               assert_conv_shape)
 from theano.tensor.signal.pool import (
-    Pool, MaxPoolGrad, AveragePoolGrad)
+    Pool, MaxPoolGrad, AveragePoolGrad, RoIPoolOp, RoIPoolGradOp)
 from . import pygpu
 from .type import (get_context, gpu_context_type, list_contexts,
                    GpuArraySharedVariable)
